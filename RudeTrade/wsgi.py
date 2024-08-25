e4cea83954d65750f47fe7c21ccc00944d8b8be4
mohitsharma012 <stayer.mohit@gmail.com>
2024-08-25 19:57:41 +0530

    for versal

--- RudeTrade/wsgi.py
+++ RudeTrade/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RudeTrade.settings')
 
 application = get_wsgi_application()
+
+# api/settings.py
+WSGI_APPLICATION = 'api.wsgi.app'
